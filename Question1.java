4de63afa2d45e7137aef4514b7e529cf98d60a6f
Shubham Garg <unknowngarg2001@gmail.com>
2024-12-21 12:36:04 +0000

    first commit

---
+++ Question1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Question1{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = sc.next();
+		System.out.print("Hello! "+name+" . Good Afternoon.");
+	}
+}
